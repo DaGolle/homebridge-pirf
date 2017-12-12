d7b6aa9458064add53b87a447aa6b6065ba77aca
DaGolle <dagolle@telenet.be>
2017-12-12 21:06:18 +0100

    First working version
    Only WindowCovering type supported

---
+++ index.js
@@ -0,0 +1,15 @@
+const PiRFPlatform = require('./lib/PiRFPlatform');
+
+module.exports = function(homebridge) {
+  // Accessory must be created from PlatformAccessory Constructor
+  Accessory = homebridge.platformAccessory;
+
+  // Service and Characteristic are from hap-nodejs
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  UUIDGen = homebridge.hap.uuid;
+  
+  // For platform plugin to be considered as dynamic platform plugin,
+  // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
+  homebridge.registerPlatform("homebridge-pirf", "PiRF", PiRFPlatform, false);
+}
